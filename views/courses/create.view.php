72753a931f5c210ce7040c4663a06a98d938eb2b
Mosky117 <davi.mosky@outlook.com>
2024-01-24 00:05:19 +0100

    little adjustments

--- views/courses/create.view.php
+++ views/courses/create.view.php
@@ -31,6 +31,7 @@
 
             <div class="sm:col-span-4">
                 <label for="subjects" class="block text-sm font-medium leading-6 text-gray-900">Subjects</label>
+                <label for="subjects" class="block text-sm font-medium leading-6 text-gray-900">Long Press CTRL and click with mouse</label>
                 <div class="mt-2">
                     <select id="subjects" name="subjects[]" multiple size="10" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         <?php 
